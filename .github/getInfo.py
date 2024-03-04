c61c8ef6e52681cac95387ea9949e609812f7c9c
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-03 18:05:30 -0600

    dfafsdfadaf

--- .github/getInfo.py
+++ .github/getInfo.py
@@ -15,5 +15,8 @@ eee = archive.read('changelog.md')
 
 
 if os.getenv('GITHUB_OUTPUT'):
-		with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
-			file.write('cl=' + ("# " + eee.split("##")[1]).replace((eee.split("##")[1]).split("\r")[0], "What's New This Update") + '\nversion=' + json.loads(archive.read('mod.json'))["version"] + '\n')
+	with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
+		file.write('cl=' + ("# " + eee.split("##")[1]).replace((eee.split("##")[1]).split("\r")[0], "What's New This Update") + '\n'
+if os.getenv('GITHUB_OUTPUT'):
+	with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
+		file.write('version=' + json.loads(archive.read('mod.json'))["version"] + '\n')version=' + json.loads(archive.read('mod.json'))["version"] + '\n')

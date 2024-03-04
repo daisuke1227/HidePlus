d7b4d82c2505fb1e7ad5a17b78da8ac1a08e5405
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-03 19:14:56 -0600

    fdsafdsafdsafdsa

--- .github/getInfo.py
+++ .github/getInfo.py
@@ -18,12 +18,12 @@ for eeeee in eee:
 
 eeeeee = ''.join(eeee)
 
-one = ('# ' + eeeeee.split('##')[1]).replace((eeeeee.split('##')[1]).split('\r')[0], 'What\'s New This Update').replace('\n', "\\n")
-two = json.loads(archive.read('mod.json'))["version"]
+one = ('# ' + eeeeee.split('##')[1]).replace((eeeeee.split('##')[1]).split('\r')[0], 'What\'s New This Update').replace('\n', "")
+two = json.loads(archive.read('mod.json'))["version"].replace("v","")
 
 if os.getenv('GITHUB_OUTPUT'):
 	with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
-		file.write(f'cl=\'{one}\'\n')
+		file.write(f'cl={one}\n')
 if os.getenv('GITHUB_OUTPUT'):
 	with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
-		file.write(f'version=\'{two}\'\n')
+		file.write(f'version={two}\n')

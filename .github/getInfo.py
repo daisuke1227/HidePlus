2df9fccd8c117eb0ba34c8d213a9c07110c56321
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-03 18:02:36 -0600

    oiupoi

--- .github/getInfo.py
+++ .github/getInfo.py
@@ -11,12 +11,9 @@ import subprocess
 
 
 archive = zipfile.ZipFile('coopeeo.hideplus.geode', 'r')
-eee = archive.open('changelog.md')
-eeee = []
-for eeeee in eee:
-	eeee.append(eeeee.decode('utf-8'))
-	eeeeee = "".join(eeee)
+eee = archive.read('changelog.md')
+
 
 if os.getenv('GITHUB_OUTPUT'):
 		with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
-			file.write('cl=' + ("# " + eeeeee.split("##")[1]).replace((eeeeee.split("##")[1]).split("\r")[0], "What's New This Update") + '\nversion=' + json.loads(archive.read('mod.json'))["version"] + '\n')
+			file.write('cl=' + ("# " + eee.split("##")[1]).replace((eee.split("##")[1]).split("\r")[0], "What's New This Update") + '\nversion=' + json.loads(archive.read('mod.json'))["version"] + '\n')
